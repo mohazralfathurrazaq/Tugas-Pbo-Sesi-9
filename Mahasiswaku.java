f21d43b414dca54973048a4153eed1511bd65d41
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-12-16 07:43:06 +0700

    Add files via upload

---
+++ Mahasiswaku.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @Moh Azral Fathurrazaq
+ */
+public class Mahasiswaku {
+    public static void main(String[] args) {
+        Mahasiswa <String,String, Integer> m = new Mahasiswa<>();
+        m.setNim("1102020");
+        m.setNama("ferdi");
+        m.setClas(21);
+        System.out.println(m.getNim());
+        System.out.println(m.getNama());
+        System.out.println(m.getClass());
+    }
+    
+}
